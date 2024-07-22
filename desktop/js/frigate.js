3138428c88c4b4afca1c69d97d74c60679430273
sagitaz <sagitaz@gmail.com>
2024-07-22 23:51:35 +0200

    Fix params url

--- desktop/js/frigate.js
+++ desktop/js/frigate.js
@@ -330,7 +330,7 @@ function printEqLogic(_eqLogic) {
 
 
         const img = $('.eqLogicAttr[data-l1key=configuration][data-l2key=img]').val().replace(/&amp;/g, '&');
-        let imgSrc = "/plugins/frigate/core/ajax/frigate.proxy.php?url=" + img;
+        let imgSrc = "/plugins/frigate/core/ajax/frigate.proxy.php?url=" + encodeURIComponent(img);
         const imgElement = document.getElementById('imgFrigate');
         let intervalId;
 
