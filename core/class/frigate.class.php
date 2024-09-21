3b99bb0bd8083c08bfaf8f808fc1eb578622cd57
sagitaz <sagitaz@gmail.com>
2024-09-21 07:04:34 +0200

    add post save

--- core/class/frigate.class.php
+++ core/class/frigate.class.php
@@ -285,6 +285,10 @@ class frigate extends eqLogic
         $infoCmd->save();
       }
     }
+
+    $infoCmd = self::createCmd($this->getId(), "SNAPSHOT LIVE", "string", "", "link_snapshot", "CAMERA_URL", 0, null, 0);
+    $infoCmd->setGeneric_type("CAMERA_URL");
+    $infoCmd->save();
   }
 
   // Fonction exécutée automatiquement avant la suppression de l'équipement
